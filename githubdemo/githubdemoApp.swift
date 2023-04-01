4afd130b84c982b66af7bd7d7b406d0402bb03d7
rashid <momras201@gmail.com>
2023-04-01 11:35:37 +0300

    Initial Commit

---
+++ githubdemo/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  githubdemo
+//
+//  Created by Mohammad Rashid on 1.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
